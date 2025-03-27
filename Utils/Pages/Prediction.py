00187e79331ccf7adf0b74d656a34953385028d5
Adhish Mazumder <107548720+AdhishMazumder@users.noreply.github.com>
2025-03-27 18:20:49 +0530

    Update Prediction.py

--- Utils/Pages/Prediction.py
+++ Utils/Pages/Prediction.py
@@ -93,33 +93,15 @@ def pred():
                         "-dir ./ -file descriptors_output_pic50_RF.csv"
                     )
 
-                    bashCommand_clf = (
-                        "java -Xms2G -Xmx2G -Djava.awt.headless=true "
-                        "-jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar "
-                        "-removesalt -standardizenitro -fingerprints "
-                        "-descriptortypes ./PaDEL-Descriptor/MACCSFingerprinter.xml "
-                        "-dir ./ -file descriptors_output_pic50_CF.csv"
-                    )
-
                     try:
                         process_reg = subprocess.Popen(bashCommand_reg, shell=True, stdout=subprocess.PIPE,
                                                    stderr=subprocess.PIPE)
-                        process_clf = subprocess.Popen(bashCommand_clf, shell=True, stdout=subprocess.PIPE,
-                                                   stderr=subprocess.PIPE)
                         output_reg, error_reg = process_reg.communicate()
-                        output_clf, error_clf = process_clf.communicate()
 
                         if process_reg.returncode == 0:
                             print("Descriptor calculation completed successfully.")
                         else:
                             print(f"Error encountered:\n{error_reg.decode('utf-8')}")
-                    except Exception as e:
-                        print(f"An error occurred: {e}")
-                        
-                        if process_clf.returncode == 0:
-                            print("Descriptor calculation completed successfully.")
-                        else:
-                            print(f"Error encountered:\n{error_clf.decode('utf-8')}")
                     except Exception as e:
                         print(f"An error occurred: {e}")
                     # os.remove('molecule.smi')
@@ -165,48 +147,14 @@ def pred():
                         st.markdown(filedownload(df), unsafe_allow_html=True)
                     return df
 
-                def build_model_clf(input_data):
-                    download = './Utils/Pictures/Download-Icon.png'
-                    downloadbutton = Image.open(download)
-
-                    # Reads in saved regression model
-                    cf_subc_ic50 = './Utils/Pages/Models/Classification/IC50/rf_maccs_model.pkl'
-                    load_model_cf = pickle.load(open(cf_subc_ic50, 'rb'))
-
-                    # Apply model to make predictions
-                    prediction_clf = load_model_cf.predict(input_data)
-
-                    st.markdown('###### **$IC_{50}$**')
-                    prediction_output_clf = pd.Series(prediction_clf, name='Classification')
-                    molecule_name = pd.Series(compound_name, name='Compound Name/ID')
-
-                    df = pd.concat([molecule_name, prediction_output_clf], axis=1)
-
-                    st.markdown(
-                        "The compound, " + str(compound_name) + " is " +
-                        "<span style='color:green'><b>" + str(prediction_output_clf[0]) + "</b></span>" +
-                        " against the target.", unsafe_allow_html=True)
-                    st.write(df)
-
-                    c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15 = st.columns(15)
-                    with c1:
-                        st.image(downloadbutton, output_format="PNG", channels="RGB", width=25)
-                    with c2:
-                        st.markdown(filedownload(df), unsafe_allow_html=True)
-                    return df
-
                 with st.spinner("PPGBioPred is calculating Bioactivity..."):
                     desc_calc()
 
                     # Read in calculated descriptors and display the dataframe
                     desc_reg = pd.read_csv('descriptors_output_pic50_RF.csv')
-                    desc_clf = pd.read_csv('descriptors_output_pic50_CF.csv')
                     # Read descriptor list used in previously built model
-                    Xlist_clf = list(pd.read_csv('./Utils/Pages/Models/Classification/IC50/df_MACCS_final_classification.csv').columns)
                     Xlist_reg = list(pd.read_csv('./Utils/Pages/Models/Regression/IC50/df_Substructure_final.csv').columns)
-                    desc_subset_clf = desc_clf[Xlist_clf]
                     desc_subset_reg = desc_reg[Xlist_reg]
                     # Apply trained model to make prediction on query compounds
                     build_model_reg(desc_subset_reg)
-                    build_model_clf(desc_subset_clf)
 
