729285966ea58377f89c46b878fc80e79892cd09
Adhish Mazumder <107548720+AdhishMazumder@users.noreply.github.com>
2025-07-23 02:52:59 +0530

    Update Homepage.py

--- Utils/Pages/Homepage.py
+++ Utils/Pages/Homepage.py
@@ -58,8 +58,8 @@ def about():
                 scientific community, and we are committed to keeping it up-to-date with the latest advances in machine 
                 learning and drug discovery.
                 </h6>
-                <h6 style='text-align: justify; color: black;font-weight: normal;'>
-                If you find value using our webserver please cite it the our paper: Adhish, M., Manjubala, I. PPGBioPred: 
+                <h6 style='text-align: justify; color: black;font-weight: bold;'>
+                If you find value using our webserver please cite our paper: Adhish, M., Manjubala, I. PPGBioPred: 
                 a webserver for predicting the bioactivity of compounds against PPARγ involved in the negative regulation of the 
                 Wnt/β-catenin signaling pathway. Mol Divers (2025). https://doi.org/10.1007/s11030-025-11297-1.
                 </h6>  
